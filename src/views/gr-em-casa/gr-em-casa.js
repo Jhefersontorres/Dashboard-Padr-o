2a0276e434d1da35d91064a11736574df2cc9861
unknown <jheferson_torres@hotmail.com>
2020-11-25 07:26:29 -0400

    commit terça

--- src/views/gr-em-casa/gr-em-casa.js
+++ src/views/gr-em-casa/gr-em-casa.js
@@ -1,21 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from "react";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
 import SideBar from "../../components/sidebar/sidebar";
+import "../../styles/grcasa.css";
 
 export default function () {
-    return (
-        <React.Fragment>
-        <div classename="container" >
-            <SideBar/>
-            <div classename="hearder">
-                <button>NOVO</button>
-                <span>GR EM CASA </span>
-            </div>
-            <div classname="cards-views">
+  //modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-            </div>
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <div classename="container">
+        <SideBar />
+        <div id="teste">
+          <i class="fa fa-user-circle"></i>
+          <p>Jheferson torres</p>
         </div>
-        </React.Fragment>
-    )
+        <Grid container justify="center">
+          <Grid
+            spacing={4}
+            alignItems="center"
+            justify="center"
+            container
+            className="grid"
+          >
+            <Grid item xs={12} md={6}>
+              <Paper className="paper">
+                <div classename="hearder">
+                  <button onClick={handleOpen}>NOVO</button>
+                  <span>GR EM CASA </span>
+                </div>
+
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className="modal"
+                  open={open}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{ timeout: 700 }}
+                >
+                  <div>
+                    <button type="submit" variant="contained" color="primary">
+                      Salvar
+                    </button>
+                    <button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleClose}
+                      BackdropProps={{ timeout: 700 }}
+                    >
+                      Voltar
+                    </button>
+                  </div>
+                </Modal>
+
+                <div classname="cards-views"></div>
+              </Paper>
+              <Paper>
+                <h1>tsteste</h1>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
 }
